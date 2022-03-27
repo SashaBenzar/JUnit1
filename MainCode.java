498606cfcecc15459f1780bc35758963581bb95e
SashaBenzar <65127188+SashaBenzar@users.noreply.github.com>
2022-03-27 22:50:51 +0300

    Create MainCode.java

---
+++ MainCode.java
@@ -0,0 +1,48 @@
+package Lab1_1;
+import java.util.Random;
+public class MainCode {
+	public static int minNum(int arr[]) {
+        int min = arr[0];
+        int minInx = 0;
+        for (int i = 0; i < arr.length; i++) {
+          if (Math.abs(arr[i]) < Math.abs(min)) {
+            min = arr[i];
+            minInx = i;
+          }
+        }
+    return minInx;
+    }
+	public static int plusNum(int arr[]) {
+        int min = 0;
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+          if (arr[i] >= 0) {
+            min = i;
+            break;
+          }
+        }
+        for (int j = arr.length-1; j > -1; j--) {
+            if (arr[j] >= 0) {
+              max = j;
+              break;
+            }
+          }
+        for (int f = min+1; f < max; f++) {
+            sum += arr[f];
+          }
+    return sum;
+    }
+	public static void main(String[] args) 
+	{
+		Random random = new Random();
+		int[] array = new int[5];
+		int min = -100;
+        int max = 100;
+		for (int i = 0; i < array.length; i++) {
+		    array[i] = random.nextInt(max + 1 - min) + min;
+		    System.out.println(array[i]);
+		}
+		System.out.println(plusNum(array));
+	}
+}
